2db8768c19d29c35f551a08b1945f78e23e1dea2
Leonardo Trojan <leo.troyan.melo@gmail.com>
2026-02-08 06:55:31 -0300

    fix: responsive problem on projects

--- src/app/components/projects.tsx
+++ src/app/components/projects.tsx
@@ -2,7 +2,7 @@ import ProjectCard from "./projectsComponents/project-card";
 
 const ProjectsSection = () => {
     return ( 
-        <div id="projects" className="min-h-screen w-full flex flex-col justify-center items-center">
+        <div id="projects" className="min-h-screen w-full flex flex-col justify-center items-center pb-30">
             <h1 className="font-bold text-3xl text-center mb-10">Meus <span className="text-green-600">projetos</span></h1>
             <div className="flex justify-center">
                 <div className="px-5 grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
